7fe8064b0e4ab0382c3824343161ef003f4241b6
balu541 <cbn1548@gmail.com>
2020-05-11 15:22:55 +0530

    Show More/Show Less

---
+++ src/app/app.component.ts
@@ -0,0 +1,221 @@
+import {Component, } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import {CommonModule, NgLocalization, NgLocaleLocalization} from '@angular/common'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  providers:[{ provide: NgLocalization, useClass: NgLocaleLocalization }]
+})
+export class AppComponent {
+  name:string;
+  findB:Boolean;
+  data = [{
+            "colorName":"red",
+            "hexValue":"#f00"
+        },
+        {
+            "colorName":"green",
+            "hexValue":"#0f0"
+        },
+        {
+            "colorName":"blue",
+            "hexValue":"#00f"
+        },
+        {
+            "colorName":"cyan",
+            "hexValue":"#0ff"
+        },
+        {
+            "colorName":"magenta",
+            "hexValue":"#f0f"
+        },
+        {
+            "colorName":"yellow",
+            "hexValue":"#ff0"
+        },
+        {
+            "colorName":"black",
+            "hexValue":"#000"
+        },{
+            "colorName":"red",
+            "hexValue":"#f00"
+        },
+        {
+            "colorName":"green",
+            "hexValue":"#0f0"
+        },
+        {
+            "colorName":"blue",
+            "hexValue":"#00f"
+        },
+        {
+            "colorName":"cyan",
+            "hexValue":"#0ff"
+        },
+        {
+            "colorName":"magenta",
+            "hexValue":"#f0f"
+        },
+        {
+            "colorName":"yellow",
+            "hexValue":"#ff0"
+        },
+        {
+            "colorName":"black",
+            "hexValue":"#000"
+        },{
+            "colorName":"red",
+            "hexValue":"#f00"
+        },
+        {
+            "colorName":"green",
+            "hexValue":"#0f0"
+        },
+        {
+            "colorName":"blue",
+            "hexValue":"#00f"
+        },
+        {
+            "colorName":"cyan",
+            "hexValue":"#0ff"
+        },
+        {
+            "colorName":"magenta",
+            "hexValue":"#f0f"
+        },
+        {
+            "colorName":"yellow",
+            "hexValue":"#ff0"
+        },
+        {
+            "colorName":"black",
+            "hexValue":"#000"
+        },{
+            "colorName":"red",
+            "hexValue":"#f00"
+        },
+        {
+            "colorName":"green",
+            "hexValue":"#0f0"
+        },
+        {
+            "colorName":"blue",
+            "hexValue":"#00f"
+        },
+        {
+            "colorName":"cyan",
+            "hexValue":"#0ff"
+        },
+        {
+            "colorName":"magenta",
+            "hexValue":"#f0f"
+        },
+        {
+            "colorName":"yellow",
+            "hexValue":"#ff0"
+        },
+        {
+            "colorName":"black",
+            "hexValue":"#000"
+        },{
+            "colorName":"red",
+            "hexValue":"#f00"
+        },
+        {
+            "colorName":"green",
+            "hexValue":"#0f0"
+        },
+        {
+            "colorName":"blue",
+            "hexValue":"#00f"
+        },
+        {
+            "colorName":"cyan",
+            "hexValue":"#0ff"
+        },
+        {
+            "colorName":"magenta",
+            "hexValue":"#f0f"
+        },
+        {
+            "colorName":"yellow",
+            "hexValue":"#ff0"
+        },
+        {
+            "colorName":"black",
+            "hexValue":"#000"
+        },{
+            "colorName":"red",
+            "hexValue":"#f00"
+        },
+        {
+            "colorName":"green",
+            "hexValue":"#0f0"
+        },
+        {
+            "colorName":"blue",
+            "hexValue":"#00f"
+        },
+        {
+            "colorName":"cyan",
+            "hexValue":"#0ff"
+        },
+        {
+            "colorName":"magenta",
+            "hexValue":"#f0f"
+        },
+        {
+            "colorName":"yellow",
+            "hexValue":"#ff0"
+        },
+        {
+            "colorName":"black",
+            "hexValue":"#000"
+        }
+    ]
+  content:any[]=new Array();
+  counter:number;
+  constructor() {
+    this.counter=0;
+    this.getData();
+    this.name = 'Angular2'
+  }
+  
+  getData(){
+    console.log(this.counter + 'dat size'+this.data.length)
+    for(let i=this.counter+1;i<this.data.length;i++)
+    {
+    this.content.push(this.data[i]);
+    if(i%10==0) break;
+    }
+    this.counter+=10;
+    if(this.counter>=this.data.length)
+    {
+      this.findB=false;
+    }
+    else
+    {
+      this.findB=true;
+    }
+  }
+  getDataLess(){
+    console.log(this.counter + 'dat size'+this.data.length)
+    for(let i=this.counter+1;i<this.data.length;i++)
+    {
+    this.content.pop();
+    if(i%10==0) break;
+    }
+    this.counter-=10;
+    if(this.counter<=0)
+    {
+      this.findB=true;
+    }
+    else
+    {
+      this.findB=false;
+    }
+  }
+}
+
